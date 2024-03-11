1a29f97f7f8c0a192d14e42e3df1d3d49eab53f0
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-03-11 23:50:41 +0530

    added some routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./routes/Home.jsx";
+
+import { Root, Home, Login, Signup, Dashboard } from "./routes";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,7 @@
+import { default as Root } from "./Root";
+import { default as Home } from "./Home";
+import { default as Login } from "./Login";
+import { default as Signup } from "./Signup";
+import { default as Dashboard } from "./Dashboard";
+
+export { Root, Home, Login, Signup, Dashboard };
