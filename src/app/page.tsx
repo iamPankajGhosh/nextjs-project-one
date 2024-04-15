61001141c58bf22151a0cc38b29abfdad81bb054
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-04-16 00:15:48 +0530

    add whychooseus section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased">
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
